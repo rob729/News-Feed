f0c89ad8b0d64f5e789ec2f1b28da71bf35dc98f
Robin Singh <robin.singh@grofers.com>
2024-02-04 14:37:01 +0530

    feat: Added news source name in news source pill view (#75)

--- app/src/main/java/com/rob729/newsfeed/ui/components/NewsSourcePill.kt
+++ app/src/main/java/com/rob729/newsfeed/ui/components/NewsSourcePill.kt
@@ -1,28 +1,37 @@
 package com.rob729.newsfeed.ui.components
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import com.rob729.newsfeed.R
+import com.rob729.newsfeed.model.ui.NewsSourceUiData
+import com.rob729.newsfeed.ui.theme.lexendDecaFontFamily
 
 @Composable
 fun NewsSourcePill(
     itemSize: Dp,
-    imageUrl: String,
+    newsSourceUiData: NewsSourceUiData,
     isSelected: Boolean,
     modifier: Modifier = Modifier,
     onClick: () -> Unit
@@ -31,24 +40,37 @@ fun NewsSourcePill(
     Column(
         modifier = modifier
             .width(itemSize)
-            .clickable(onClick = onClick),
+            .clip(RoundedCornerShape(8.dp))
+            .clickable(onClick = onClick)
+            .background(if (isSelected) MaterialTheme.colorScheme.secondaryContainer else Color.Transparent),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
+        Spacer(modifier = Modifier.height(4.dp))
         AsyncImage(
-            model = imageUrl,
+            model = newsSourceUiData.imageUrl,
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .clip(CircleShape)
                 .height(75.dp)
                 .width(75.dp)
-                .border(
-                    border = if (isSelected) BorderStroke(
-                        4.dp,
-                        colorResource(R.color.status_bar)
-                    ) else BorderStroke(0.dp, Color.Transparent), shape = CircleShape
-                )
         )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = newsSourceUiData.name,
+            maxLines = 2,
+            minLines = 2,
+            fontWeight = FontWeight.Normal,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 2.dp),
+            textAlign = TextAlign.Center,
+            overflow = TextOverflow.Ellipsis,
+            fontFamily = lexendDecaFontFamily,
+            fontSize = 12.sp,
+            lineHeight = 16.sp
+        )
+        Spacer(modifier = Modifier.height(4.dp))
     }
 }
