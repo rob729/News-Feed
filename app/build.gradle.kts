acd7e19fdb48ba6a1a8f142dee1642d7533fffd3
Robin Singh <rs729028@gmail.com>
2024-06-09 13:51:25 +0530

    Version bumped from 1.2.0 to 1.2.1 (#149)
    Co-authored-by: github-actions <github-actions@github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         applicationId = "com.rob729.newsfeed"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
